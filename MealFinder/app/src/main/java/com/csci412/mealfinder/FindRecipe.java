a779bcf767b27cd9a0e4b66df4a5ad469dafeb57
moksnej <moksnej@wwu.edu>
2019-05-13 21:53:05 -0700

    Replace FindRecipe.java

--- MealFinder/app/src/main/java/com/csci412/mealfinder/FindRecipe.java
+++ MealFinder/app/src/main/java/com/csci412/mealfinder/FindRecipe.java
@@ -1,5 +1,9 @@
 package com.csci412.mealfinder;
 
+import android.support.constraint.ConstraintSet;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
@@ -12,5 +16,25 @@ public class FindRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_recipe);
+
+        setContentView(R.layout.find_recipe);
+        final EditText ing4 = (EditText) findViewById(R.id.forth_ingredient);
+        ing4.setVisibility(View.INVISIBLE);
+        final EditText ing5 = (EditText) findViewById(R.id.fifth_ingredient);
+        ing5.setVisibility(View.INVISIBLE);
+        final Button addButton = (Button) findViewById(R.id.add_ingredient_button);
+
+        Button add = (Button) findViewById(R.id.add_ingredient_button);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(ing4.getVisibility() == View.INVISIBLE) {
+                    ing4.setVisibility(View.VISIBLE);
+                } else {
+                    ing5.setVisibility(View.VISIBLE);
+                    addButton.setVisibility(View.INVISIBLE);
+                }
+            }
+
+        });
     }
 }
