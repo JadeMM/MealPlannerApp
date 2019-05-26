7b0f617185d77af5c4f2e633b35fcda179f556dd
moksnej <moksnej@wwu.edu>
2019-05-25 22:26:19 -0700

    Shows list of recipes

--- MealFinder/app/src/main/java/com/csci412/mealfinder/MainPage.java
+++ MealFinder/app/src/main/java/com/csci412/mealfinder/MainPage.java
@@ -6,12 +6,40 @@ import android.widget.*;
 import android.view.View;
 import android.content.Intent;
 
+import java.util.ArrayList;
+
 public class MainPage extends AppCompatActivity {
+
+    private ListView obj;
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+
+        mydb = new DBHelper(this);
+        ArrayList array_list = mydb.getAllCotacts();
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, array_list);
+
+        obj = (ListView)findViewById(R.id.listView1);
+        obj.setAdapter(arrayAdapter);
+        obj.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+                // TODO Auto-generated method stub
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(),DisplayCreatedRecipes.class);
+
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
 
         Button next = (Button) findViewById(R.id.find_button);
         next.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +54,15 @@ public class MainPage extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), AddRecipe.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
+        Button custom = (Button) findViewById(R.id.view_custom_button);
+        custom.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), DisplayCreatedRecipes.class);
                 startActivityForResult(myIntent, 0);
             }
 
