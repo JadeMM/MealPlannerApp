91e9eec5175ea2d4830e1e784a7e60523dd16fa5
moksnej <moksnej@wwu.edu>
2019-05-28 13:34:27 -0700

    Altered layout

--- MealFinder/app/src/main/java/com/csci412/mealfinder/FindRecipe.java
+++ MealFinder/app/src/main/java/com/csci412/mealfinder/FindRecipe.java
@@ -18,21 +18,30 @@ public class FindRecipe extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_recipe);
 
-        setContentView(R.layout.find_recipe);
+
+        final EditText ing1 = (EditText) findViewById(R.id.first_ingredient);
+        final EditText ing2 = (EditText) findViewById(R.id.second_ingredient);
+        ing2.setVisibility(View.GONE);
+        final EditText ing3 = (EditText) findViewById(R.id.third_ingredient);
+        ing3.setVisibility(View.GONE);
         final EditText ing4 = (EditText) findViewById(R.id.forth_ingredient);
-        ing4.setVisibility(View.INVISIBLE);
+        ing4.setVisibility(View.GONE);
         final EditText ing5 = (EditText) findViewById(R.id.fifth_ingredient);
-        ing5.setVisibility(View.INVISIBLE);
+        ing5.setVisibility(View.GONE);
         final Button addButton = (Button) findViewById(R.id.add_ingredient_button);
 
-        Button add = (Button) findViewById(R.id.add_ingredient_button);
+        final Button add = (Button) findViewById(R.id.add_ingredient_button);
         add.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(ing4.getVisibility() == View.INVISIBLE) {
+                if(ing2.getVisibility() == View.GONE) {
+                    ing2.setVisibility(View.VISIBLE);
+                } else if (ing3.getVisibility() == View.GONE){
+                    ing3.setVisibility(View.VISIBLE);
+                } else if (ing4.getVisibility() == View.GONE){
                     ing4.setVisibility(View.VISIBLE);
                 } else {
                     ing5.setVisibility(View.VISIBLE);
-                    addButton.setVisibility(View.INVISIBLE);
+                    add.setVisibility(View.GONE);
                 }
             }
 
